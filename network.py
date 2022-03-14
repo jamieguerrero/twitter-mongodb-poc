73d869b44a3de5e1cee223a0dfd5b4052423e515
Patrick Hewan <patrickhewan@hotmail.com>
2022-03-14 14:40:34 -0400

    visualization

--- network.py
+++ network.py
@@ -12,7 +12,7 @@ from http.client import BadStatusLine
 import nltk
 import numpy as np
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
+import matplotlib.pyplot as plt 
 
 load_dotenv()
 
@@ -131,18 +131,35 @@ csv_file = df1.to_csv(f"data/test2.csv", index=None)
 
 network = pd.DataFrame(zipped_result)
 
-print(network)
+# print(user)
+
+# network['retweetuser']=network['retweetuser'].str.replace('(','')
+# print(network)
 
 test = nx.from_pandas_edgelist(
     network,
-    source = user,
-    target = retweetuser,
+    source = 'user',
+    target = 'retweetuser',
     create_using = nx.DiGraph()
 ) 
 
+print('Nodes in RT network:', len(test.nodes()))
 
+# Create random layout positions
+pos = nx.random_layout(test)
 
+# Create size list
+sizes = [x[1] for x in test.degree()]
 
+# Draw the network
+nx.draw_networkx(test, pos, 
+    with_labels = False, 
+    node_size = sizes,
+    width = 0.1, alpha = 0.7,
+    arrowsize = 2, linewidths = 0)
+
+# Turn axis off and show
+plt.axis('off'); plt.show(); plt.savefig('hi.png')
 
 
 
